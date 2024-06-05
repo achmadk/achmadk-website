f7da831a5c5e6cbb5db6cd867ed340df8b47688c
Achmad Kurnianto <achmad_kurnianto@yahoo.co.id>
2024-06-05 16:12:52 +0700

    feat(assets): :lipstick: add icons for web manifest, update packages
    add web icons from icon kitchen (icon generator for apps). update astro configuration.

--- astro.config.ts
+++ astro.config.ts
@@ -8,10 +8,6 @@ import AstroCompressor from 'astro-compressor';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-import { removeReactDevtools  } from '@achmadk/vite-plugin-react-remove-devtools'
-
-const isProduction = process.env.NODE_ENV === 'production'
-
 // https://astro.build/config
 export default defineConfig({
   site: process.env.BLOG_URL ?? "https://achmadk-dev.vercel.app",
@@ -28,14 +24,30 @@ export default defineConfig({
       injectManifest: {
         globPatterns: ['**/*.{css,js,png,webp,svg,woff,woff2,html,avif}'],
         rollupFormat: 'iife',
-      }
+      },
+      manifest: {
+        name: 'Achmad Kurnianto',
+        description: "Achmad Kurnianto's website",
+        short_name: 'achmadk',
+        orientation: 'portrait',
+        theme_color: '#13131f',
+        background_color: '#13131f',
+        icons: [
+          { "src": "/favicon.ico", "type": "image/x-icon", "sizes": "16x16 32x32" },
+          { "src": "/icon-192.png", "type": "image/png", "sizes": "192x192" },
+          { "src": "/icon-512.png", "type": "image/png", "sizes": "512x512" },
+          { "src": "/icon-192-maskable.png", "type": "image/png", "sizes": "192x192", "purpose": "maskable" },
+          { "src": "/icon-512-maskable.png", "type": "image/png", "sizes": "512x512", "purpose": "maskable" }
+        ],
+        protocol_handlers: [
+          {
+            protocol: 'web+achmadkblogs',
+            url: '/blogs/%s'
+          }
+        ]
+      },
     }),
     AstroCompress(),
     AstroCompressor()
   ],
-  vite: {
-    plugins: [
-      ...(isProduction ? [removeReactDevtools()] : [])
-    ]
-  }
 });
